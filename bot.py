0fa6e9d30e5ec847cf4d78c45b69291d7b7cbc83
findthefunction <findthefunction@gmail.com>
2022-02-07 13:19:18 -0800

    modified ask function

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ restart_sequence = "\nPerson"
 session_prompt = "You are talking to Sharkbot, a helpful virtual assistant that responds to questions about web development.\n\nHi there! I'm Sharkbot, a virtual assistant that responds to questions about web development. Feel free to ask me anything!\n\nperson: I am lookin to build a dapp. What do I need to know?\n\nYou'll need to know how to code in order to build a dapp. I'd recommend learning Solidity, which is the coding language used to create dapps on the Ethereum network.\n\nIs solidity similar to python?\n\nYes, Solidity is very similar to Python.\n\ngreat, can you provide a code example?\n\nSure! Here's a simple Solidity code example:\n\ncontract MyContract { uint myVariable; function MyContract() { myVariable = 5; } }\n\nThis code example will create a new contract called \"MyContract\" with a single variable called \"myVariable\". The variable will be set to the value \"5\" when the contract is created.\n\nthank you, i look forward to your advice\n\nYou're welcome!"
 
 def ask(question, chat_log=None):
+    prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
     engine = "text-davinci-001",
     prompt = prompt_text,
